2cd910783466b8a7f701fb1ad6e3e27daab47108
Fazzy33 <fazzy4336j@gmail.com>
2026-02-06 14:24:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def get_answer(question):
         "company": "PLACEMENT COMPANIES:",
         "contact": "CONTACT:",
         "phone": "CONTACT:",
+        "cutoff": "CUTOFF:",
         "office": "OFFICE HOURS:",
         "timing": "OFFICE HOURS:",
         "other websites": "OTHER WEBSITES:",
